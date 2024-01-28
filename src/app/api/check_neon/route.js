2b757b4039840c36f7f40318e53e11ed267e3fdd
Muhammad Abuar <muhammadabuzar010@gmail.com>
2024-01-28 14:59:37 +0500

    Neon Check

---
+++ src/app/api/check_neon/route.js
@@ -0,0 +1,26 @@
+import { NextResponse } from "next/server";
+const { Pool } = require("pg");
+
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false,
+  },
+});
+
+export async function GET(req) {
+  try {
+    console.log(process.env.DATABASE_URL);
+    pool.query("SELECT * FROM users ORDER BY id ASC", (error, results) => {
+      if (error) {
+        throw error;
+      }
+      response.status(200).json(results.rows);
+    });
+  } catch (error) {
+    return NextResponse.json(
+      { success: false, error: error.message },
+      { status: 500 }
+    );
+  }
+}
---
+++ src/components/Experimental/NeonCheck.jsx
@@ -0,0 +1,30 @@
+"use client";
+
+import { useState } from "react";
+
+export default function Neon() {
+  const [data, setData] = useState("");
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const response = await fetch("/api/check_neon", {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+        },
+      });
+      if (response.ok) {
+        const data = await response.json();
+        console.log(data);
+        setData(data);
+      }
+    } catch (error) {
+      console.error("Error submitting form:", error.message);
+    }
+  };
+  return (
+    <div>
+      <button onClick={handleSubmit}>Fetch DATA</button>
+    </div>
+  );
+}
