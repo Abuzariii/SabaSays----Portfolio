b6c749b043ce5e5356b8d23f355af6d294b370c9
Muhammad Abuar <muhammadabuzar010@gmail.com>
2024-01-28 15:05:26 +0500

    Neon check 3.0

--- src/app/api/check_neon/route.js
+++ src/app/api/check_neon/route.js
@@ -12,10 +12,8 @@ export async function GET(req) {
   try {
     console.log(process.env.DATABASE_URL);
     pool.query("SELECT * FROM users ORDER BY id ASC", (error, results) => {
-      if (error) {
-        throw error;
-      }
       response.status(200).json(results.rows);
+      return NextResponse.json(results.rows).status(200);
     });
   } catch (error) {
     return NextResponse.json(
