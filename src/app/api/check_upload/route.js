53868d6e2d8a74c5ab6268cbd2e39043601c2c0a
Muhammad Abuar <muhammadabuzar010@gmail.com>
2024-01-30 01:30:42 +0500

    POST worked

--- src/app/api/check_upload/route.js
+++ src/app/api/check_upload/route.js
@@ -1,5 +1,5 @@
 // import multer from "multer";
-// import { NextResponse } from "next/server";
+import { NextResponse } from "next/server";
 
 // const upload = multer({
 //   storage: multer.diskStorage({
@@ -28,3 +28,22 @@
 //     );
 //   }
 // }
+
+import getStream from "get-stream";
+
+export async function POST(req) {
+  try {
+    // const bodyBuffer = await getStream(req.body);
+    // const bodyObject = JSON.parse(bodyBuffer.toString());
+    // const { name, gender } = bodyObject;
+    // console.log(name, gender);
+    const data = await req.json();
+    console.log(data);
+    return NextResponse.json({ message: "Data Received" }, { status: 200 });
+  } catch (error) {
+    return NextResponse.json(
+      { success: false, error: error.message },
+      { status: 500 }
+    );
+  }
+}
--- src/components/Experimental/ImageUpload.jsx
+++ src/components/Experimental/ImageUpload.jsx
@@ -1,32 +1,51 @@
 "use client";
-import React, { useEffect, useState } from "react";
+// import React, { useEffect, useState } from "react";
 
 const ImageUpload = () => {
-  const [file, setFile] = useState("");
+  // const [file, setFile] = useState("");
 
-  const handleFileChange = (e) => {
-    setFile(e.target.files[0]);
+  // const handleFileChange = (e) => {
+  //   setFile(e.target.files[0]);
+  // };
+
+  // const handleUpload = async (file) => {
+  //   try {
+  //     const response = await fetch("/api/check_upload", {
+  //       method: "POST",
+  //       body: formData,
+  //     });
+
+  //     const data = await response.json();
+  //     console.log("Image uploaded successfully:", data);
+  //   } catch (error) {
+  //     console.error("Error uploading image:", error);
+  //   }
+  // };
+  const a = {
+    name: "abuzar",
+    gender: "male",
   };
+  const postHandler = async (e) => {
+    e.preventDefault();
 
-  const handleUpload = async (file) => {
-    try {
-      const response = await fetch("/api/chec_upload", {
-        method: "POST",
-        body: formData,
-      });
+    const response = await fetch("/api/check_upload", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ name: "abuzar", gender: "male" }),
+    });
 
-      const data = await response.json();
-      console.log("Image uploaded successfully:", data);
-    } catch (error) {
-      console.error("Error uploading image:", error);
-    }
+    const data = await response.json();
+    console.log(data);
   };
-
   return (
     <div>
       <h1>Upload Your Img Here</h1>
-      <input type="file" onChange={handleFileChange} />
-      <button onClick={handleUpload}>Upload</button>
+      {/* <input type="file" onChange={handleFileChange} />
+      <button onClick={handleUpload}>Upload</button> */}
+
+      <button onClick={postHandler}> SEND POST REQ</button>
     </div>
   );
 };
