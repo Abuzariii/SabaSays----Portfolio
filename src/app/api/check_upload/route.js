ff02e2d930c14b868e82db357d119f951de81ab5
Muhammad Abuar <muhammadabuzar010@gmail.com>
2024-01-30 01:59:58 +0500

    POST Image failed

--- src/app/api/check_upload/route.js
+++ src/app/api/check_upload/route.js
@@ -1,42 +1,7 @@
-// import multer from "multer";
 import { NextResponse } from "next/server";
-
-// const upload = multer({
-//   storage: multer.diskStorage({
-//     destination: "./public/uploads",
-//     filename: function (req, file, cb) {
-//       cb(null, file.originalname);
-//     },
-//   }),
-// });
-
-// export const config = {
-//   api: {
-//     bodyParser: false,
-//   },
-// };
-
-// export async function POST(req) {
-//   try {
-//     upload.single("file");
-//     const filename = req.file ? req.file.originalname : null;
-//     return NextResponse.json({ message: filename }, { status: 200 });
-//   } catch (error) {
-//     return NextResponse.json(
-//       { success: false, error: error.message },
-//       { status: 500 }
-//     );
-//   }
-// }
-
-import getStream from "get-stream";
 
 export async function POST(req) {
   try {
-    // const bodyBuffer = await getStream(req.body);
-    // const bodyObject = JSON.parse(bodyBuffer.toString());
-    // const { name, gender } = bodyObject;
-    // console.log(name, gender);
     const data = await req.json();
     console.log(data);
     return NextResponse.json({ message: "Data Received" }, { status: 200 });
