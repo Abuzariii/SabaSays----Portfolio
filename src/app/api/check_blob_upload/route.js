532dd1b2b0f2b30a83ca49724973a9ae822cc776
Muhammad Abuar <muhammadabuzar010@gmail.com>
2024-01-30 19:17:29 +0500

    CRUD Vercel Blob

--- src/app/api/check_blob_upload/route.js
+++ src/app/api/check_blob_upload/route.js
@@ -1,6 +1,9 @@
 import { NextResponse } from "next/server";
-import { put } from "@vercel/blob";
+import { put, del, list } from "@vercel/blob";
 
+const token = process.env.VERCEL_BLOB_READ_WRITE_TOKEN;
+
+// Upload a single blob
 export async function POST(req) {
   const file = req.body || "";
   const contentType = req.headers.get("content-type") || "text/plain";
@@ -8,12 +11,25 @@ export async function POST(req) {
   const filename = `${timestamp}.${contentType.split("/")[1]}`;
   console.log(filename);
 
-  const token = process.env.VERCEL_BLOB_READ_WRITE_TOKEN;
-
   const blob = await put(filename, file, {
     contentType,
     access: "public",
     token,
   });
   return NextResponse.json({ blob });
+}
+
+// Get all Blobs
+export async function GET(req) {
+  const { blobs } = await list({ token });
+  return NextResponse.json({ blobs });
+}
+
+// Delete a blob
+
+export async function DELETE(req) {
+  const data = await req.json();
+  console.log(data.url);
+  await del(data.url, { token });
+  return NextResponse.json({ message: "Deleted Succesfully" });
 }
--- src/components/Experimental/BlobImageUpload.jsx
+++ src/components/Experimental/BlobImageUpload.jsx
@@ -43,10 +43,47 @@ export default function BlobUpload() {
     });
   }
 
+  async function handleBlobList(e) {
+    e.preventDefault();
+
+    fetch("/api/check_blob_upload", {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    }).then(async (res) => {
+      if (res.status === 200) {
+        const blobs = await res.json();
+        console.log(blobs.blobs);
+      }
+    });
+  }
+
+  async function handleDeleteBlob(e) {
+    e.preventDefault();
+
+    fetch("/api/check_blob_upload", {
+      method: "DELETE",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: {
+        url: "https://cs4iqiq9bnopbxaa.public.blob.vercel-storage.com/1706614992927-tA0eVIJRyUMB4o2WGJJKSulWesCQkW.jpeg",
+      },
+    }).then(async (res) => {
+      if (res.status === 200) {
+        const data = await res.json();
+        console.log(data.message);
+      }
+    });
+  }
   return (
     <div>
       <input type="file" onChange={onChangePicture} />
       <button onClick={handleSubmit}>Upload</button>
+
+      <button onClick={handleBlobList}>GET ALL BLOBS</button>
+      <button onClick={handleDeleteBlob}>DELETE A BLOB</button>
     </div>
   );
 }
