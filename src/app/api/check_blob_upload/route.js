5381b5d81d9dce665173dff0553e69f4732b53ac
Muhammad Abuar <muhammadabuzar010@gmail.com>
2024-01-31 02:04:44 +0500

    Vercel Env Variables check 2.0

--- src/app/api/check_blob_upload/route.js
+++ src/app/api/check_blob_upload/route.js
@@ -1,7 +1,7 @@
 import { NextResponse } from "next/server";
 import { put, del, list } from "@vercel/blob";
 
-const token = process.env.VERCEL_BLOB_READ_WRITE_TOKEN;
+// const token = process.env.VERCEL_BLOB_READ_WRITE_TOKEN;
 
 // Upload a single blob
 export async function POST(req) {
@@ -14,14 +14,14 @@ export async function POST(req) {
   const blob = await put(filename, file, {
     contentType,
     access: "public",
-    token,
+    // token,
   });
   return NextResponse.json({ blob });
 }
 
 // Get all Blobs
 export async function GET(req) {
-  const { blobs } = await list({ token });
+  const { blobs } = await list({});
   return NextResponse.json({ blobs });
 }
 
@@ -30,6 +30,6 @@ export async function GET(req) {
 export async function DELETE(req) {
   const data = await req.json();
   console.log(data.url);
-  await del(data.url, { token });
+  await del(data.url, {});
   return NextResponse.json({ message: "Deleted Succesfully" });
 }
