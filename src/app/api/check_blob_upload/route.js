7271fbd2b62d2fe06dd9fca4f7829e07ff734b53
Muhammad Abuar <muhammadabuzar010@gmail.com>
2024-01-31 02:14:02 +0500

    Web analytics

--- src/app/api/check_blob_upload/route.js
+++ src/app/api/check_blob_upload/route.js
@@ -1,8 +1,6 @@
 import { NextResponse } from "next/server";
 import { put, del, list } from "@vercel/blob";
 
-// const token = process.env.VERCEL_BLOB_READ_WRITE_TOKEN;
-
 // Upload a single blob
 export async function POST(req) {
   const file = req.body || "";
@@ -14,14 +12,13 @@ export async function POST(req) {
   const blob = await put(filename, file, {
     contentType,
     access: "public",
-    // token,
   });
   return NextResponse.json({ blob });
 }
 
 // Get all Blobs
 export async function GET(req) {
-  const { blobs } = await list({});
+  const { blobs } = await list();
   return NextResponse.json({ blobs });
 }
 
@@ -30,6 +27,6 @@ export async function GET(req) {
 export async function DELETE(req) {
   const data = await req.json();
   console.log(data.url);
-  await del(data.url, {});
+  await del(data.url);
   return NextResponse.json({ message: "Deleted Succesfully" });
 }
